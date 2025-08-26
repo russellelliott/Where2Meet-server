8d4542c91cea8e65f1c3da2de84435d18947b26b
Russell Elliott <russell@quadsquad.net>
2025-08-25 19:16:47 -0700

    implemented map sharing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 dotenv.config();
+
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import emailRoutes from './email/emailRoutes.js';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDef from './swaggerDef.js';
 
 dotenv.config();
 
+
 const app = express();
+
+// Allow CORS for frontend
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        process.env.CLIENT_BASE_URL
+    ],
+    credentials: true
+}));
+
 app.use(express.json());
 
 // Swagger UI setup
